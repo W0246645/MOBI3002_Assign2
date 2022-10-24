248bbcd69999d873921f611b10c9034fbf5375ca
Matthew Covey <w0246645@campus.nscc.ca>
2022-10-24 15:38:02 -0300

    Layouts all done, need to fix the background switching on QuizScreen

--- QuizBuilder/app/src/main/java/com/example/quizbuilder/MainActivity.java
+++ QuizBuilder/app/src/main/java/com/example/quizbuilder/MainActivity.java
@@ -2,13 +2,70 @@ package com.example.quizbuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_start;
+    TextView txt_highscores;
+    EditText editText_name;
+
+    String highscores;
+    String name;
+    ArrayList<String> defs = new ArrayList<>(Arrays.asList("d1", "d2", "d3", "d4", "d5"));
+    ArrayList<String> terms = new ArrayList<>(Arrays.asList("t1", "t2", "t3", "t4", "t5"));
+    HashMap<String, String> map = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getIntent() != null) {
+            highscores = getIntent().getStringExtra("highscores");
+            name = getIntent().getStringExtra("name");
+        }
+
+        btn_start = findViewById(R.id.btn_start);
+        txt_highscores = findViewById(R.id.txt_highscores);
+        editText_name = findViewById(R.id.editText_name);
+
+        btn_start.setOnClickListener(onSubmitClicked);
+
+        txt_highscores.setText(highscores);
+        editText_name.setText(name);
+
+        for (int i = 0; i < defs.size(); i++) {
+            map.put(defs.get(i), terms.get(i));
+        }
     }
+
+    public View.OnClickListener onSubmitClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (editText_name.getText().toString().trim().length() > 0) {
+                Intent i = new Intent(MainActivity.this, QuizScreen.class);
+                i.putExtra("name", editText_name.getText().toString());
+                i.putExtra("defs", defs);
+                i.putExtra("terms", terms);
+                i.putExtra("map", map);
+                i.putExtra("highscores", highscores);
+                startActivity(i);
+            } else {
+                Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake);
+                editText_name.startAnimation(shake);
+            }
+        }
+    };
 }
--- QuizBuilder/app/src/main/java/com/example/quizbuilder/QuizScreen.java
+++ QuizBuilder/app/src/main/java/com/example/quizbuilder/QuizScreen.java
@@ -2,13 +2,53 @@ package com.example.quizbuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizScreen extends AppCompatActivity {
+    Button btn_quit, btn_restart, btn_submit, btn_term1, btn_term2, btn_term3, btn_term4;
+    TextView txt_def;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_screen);
+
+        btn_quit = findViewById(R.id.btn_quit);
+        btn_restart = findViewById(R.id.btn_restart);
+        btn_submit = findViewById(R.id.btn_submit);
+        btn_term1 = findViewById(R.id.btn_term1);
+        btn_term2 = findViewById(R.id.btn_term2);
+        btn_term3 = findViewById(R.id.btn_term3);
+        btn_term4 = findViewById(R.id.btn_term4);
+
+        txt_def = findViewById(R.id.txt_definition);
+
+        btn_quit.setOnClickListener(onQuitClicked);
+
+        btn_term1.setOnClickListener(onTermClicked);
+        btn_term2.setOnClickListener(onTermClicked);
+        btn_term3.setOnClickListener(onTermClicked);
+        btn_term4.setOnClickListener(onTermClicked);
     }
+
+    public View.OnClickListener onQuitClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(QuizScreen.this, MainActivity.class);
+            i.putExtra("name", getIntent().getStringExtra("name"));
+            i.putExtra("highscores", getIntent().getStringExtra("highscores"));
+            startActivity(i);
+        }
+    };
+
+    public View.OnClickListener onTermClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            view.setBackground(getDrawable(R.drawable.rounded_corners_selected));
+        }
+    };
 }
